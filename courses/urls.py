b1df202d37ab1195fa92049237216d4cea04b7ff
Obsidian <ObsidianRock@users.noreply.github.com>
2017-04-18 14:01:18 +0100

    formset for course modules

--- courses/urls.py
+++ courses/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
 
     url(r'^(?P<pk>\d+)/delete/$',views.CourseDeleteView.as_view(),
         name='course_delete'),
+    url(r'^(?P<pk>\d+)/module/$', views.CourseModuleUpdateView.as_view(),
+        name='course_module_update'),
 ]
